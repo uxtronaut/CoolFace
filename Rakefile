5a0de2c427f1885ee878211e32155873de1dd23f
Zach Payne <uxtronaut@gmail.com>
2015-05-26 00:00:05 -0600

    Add about window, bump version to v0.0.5

--- Rakefile
+++ Rakefile
@@ -14,11 +14,10 @@ require 'motion-yaml'
 Motion::Project::App.setup do |app|
   # Use `rake config' to see complete project settings.
   app.name = 'CoolFace'
-
   app.icon = 'face.icns'
-
   app.frameworks << 'Carbon'
   app.vendor_project 'vendor/DDHotKey', :static, :cflags => '-fobjc-arc'
-
+  app.vendor_project 'vendor/NSAttributedString+Hyperlink', :static, :cflags => '-fobjc-arc'
   app.info_plist['LSUIElement'] = true
+  app.version = '0.0.5'
 end
--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -29,6 +29,11 @@ class AppDelegate
     menu.initWithTitle 'CoolFace'
 
     mi = NSMenuItem.new
+    mi.title = 'About'
+    mi.action = 'showAbout'
+    menu.addItem mi
+
+    mi = NSMenuItem.new
     mi.title = 'Quit'
     mi.action = 'terminate:'
     menu.addItem mi
@@ -68,4 +73,115 @@ class AppDelegate
     @statusBarItem.setTitle(face)
   end
 
+  def showAbout
+    unless @aboutWindow
+      @aboutWindow = setupAboutWindow
+      @aboutWindow.contentView.addSubview(setupTitleText)
+      @aboutWindow.contentView.addSubview(setupVersionText)
+      @aboutWindow.contentView.addSubview(setupInstructionsTextA)
+      @aboutWindow.contentView.addSubview(setupInstructionsTextB)
+      @aboutWindow.contentView.addSubview(setupCreditsText)
+      @aboutWindow.contentView.addSubview(setupTransformationImage)
+    end
+
+    @aboutWindow.orderFrontRegardless
+    NSApp.activateIgnoringOtherApps(true)
+  end
+
+  def setupAboutWindow
+    width = 280
+    height = 420
+    left = NSScreen.mainScreen.frame.size.width/2 - width/2
+    top = NSScreen.mainScreen.frame.size.height/2 - height/2
+
+    window = NSPanel.alloc.initWithContentRect([[left, top], [width, height]],
+      styleMask: NSTitledWindowMask|NSClosableWindowMask,
+      backing: NSBackingStoreBuffered,
+      defer: false
+    )
+
+    window
+  end
+
+  def setupTitleText
+    text = setupTextField('CoolFace', :top => 70)
+    text.font = NSFont.systemFontOfSize(30)
+    text
+  end
+
+  def setupVersionText
+    setupTextField("Version #{NSBundle.mainBundle.infoDictionary['CFBundleVersion']}", :top => 120)
+  end
+
+  def setupInstructionsTextA
+    setupTextField("Next Cool Face: Cmd + Ctrl + Shift + C", :top => 160)
+  end
+
+  def setupInstructionsTextB
+    setupTextField("Previous Cool Face: Cmd + Ctrl + Shift + X", :top => 180)
+  end
+
+  def setupCreditsText
+    text = NSTextField.alloc.initWithFrame([[15, @aboutWindow.frame.size.height - 280], [@aboutWindow.frame.size.width - 30, 100]])
+    text.backgroundColor = @aboutWindow.backgroundColor
+    text.allowsEditingTextAttributes = true
+    text.selectable = true
+    text.editable = false
+    text.bezeled = false
+
+    credits = NSMutableAttributedString.alloc.init
+
+    credits.appendAttributedString(NSAttributedString.alloc.initWithString('Written by '))
+    url = NSURL.URLWithString('http://twitter.com/uxtronaut')
+    credits.appendAttributedString(NSAttributedString.hyperlinkFromString('@uxtronaut', withURL:url))
+
+    credits.appendAttributedString(NSAttributedString.alloc.initWithString('. Icon by '))
+    url = NSURL.URLWithString('http://twitter.com/benfrederick')
+    credits.appendAttributedString(NSAttributedString.hyperlinkFromString('@benfrederick', withURL:url))
+
+    credits.appendAttributedString(NSAttributedString.alloc.initWithString('. Cool hair animated graphic by Kole Kostelic'))
+
+    credits.appendAttributedString(NSAttributedString.alloc.initWithString('. Cool face curation assisted by Andy Mikulski'))
+
+    credits.appendAttributedString(NSAttributedString.alloc.initWithString('. Additional testing, ideas, and inspiration from the fine folks of '))
+    url = NSURL.URLWithString('http://mondorobot.com')
+    credits.appendAttributedString(NSAttributedString.hyperlinkFromString('Mondo Robot', withURL:url))
+
+    credits.appendAttributedString(NSAttributedString.alloc.initWithString('.'))
+
+    text.attributedStringValue = credits
+
+    text
+  end
+
+  def setupTextField(value, options={})
+    defaultOptions = {
+      :alignment => NSCenterTextAlignment,
+      :left => 0,
+      :top => 0,
+      :width => @aboutWindow.frame.size.width,
+      :height => 35
+    }
+
+    options = defaultOptions.merge(options)
+
+    options[:top] = @aboutWindow.frame.size.height - options[:top]
+
+    text = NSTextField.alloc.initWithFrame([[options[:left], options[:top]], [options[:width], options[:height]]])
+    text.stringValue = value
+    text.alignment = options[:alignment]
+    text.editable = false
+    text.bezeled = false
+    text.backgroundColor = @aboutWindow.backgroundColor
+
+    text
+  end
+
+  def setupTransformationImage
+    image = NSImage.alloc.initWithContentsOfFile(File.join(NSBundle.mainBundle.resourcePath, 'transformation.gif'))
+    imageView = NSImageView.alloc.initWithFrame([[0, -25], [@aboutWindow.frame.size.width, 200]])
+    imageView.image = image
+    imageView.editable = false
+    imageView
+  end
 end
